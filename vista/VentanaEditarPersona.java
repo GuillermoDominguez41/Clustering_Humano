1f2bde7cc8b91dcb67cfe6fd20f4c3f61467e202
GuillermoDominguez41 <83877647+GuillermoDominguez41@users.noreply.github.com>
2023-10-04 20:38:34 -0300

    Update VentanaEditarPersona.java

--- vista/VentanaEditarPersona.java
+++ vista/VentanaEditarPersona.java
@@ -36,7 +36,7 @@ public class VentanaEditarPersona extends JFrame {
 		rangoMinimo = coordinador.obtenerRangoMinimo();
 		rangoMaximo = coordinador.obtenerRangoMaximo();
 		
-		setSize(515, 300);
+		setSize(515, 260);
 		setTitle("Editar Persona");
 		getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
 		setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/img/agregarPersona32.png")));
@@ -85,56 +85,57 @@ public class VentanaEditarPersona extends JFrame {
 
 		JLabel lbl_Id = new JLabel("ID");
 		lbl_Id.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lbl_Id.setBounds(52, 39, 133, 22);
+		lbl_Id.setBounds(53, 40, 133, 22);
 		pnl_AgregarPersona.add(lbl_Id);
 		
 		JLabel lbl_Nombre = new JLabel("Nombre");
 		lbl_Nombre.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lbl_Nombre.setBounds(52, 39, 133, 22);
+		lbl_Nombre.setBounds(53, 68, 133, 22);
 		pnl_AgregarPersona.add(lbl_Nombre);
 		
 		JLabel lbl_IntDeporte = new JLabel("Interes Deporte");
 		lbl_IntDeporte.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lbl_IntDeporte.setBounds(52, 66, 133, 22);
+		lbl_IntDeporte.setBounds(53, 95, 133, 22);
 		pnl_AgregarPersona.add(lbl_IntDeporte);
 
 		JLabel lbl_IntMusica = new JLabel("Interes Musica");
 		lbl_IntMusica.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lbl_IntMusica.setBounds(52, 93, 133, 22);
+		lbl_IntMusica.setBounds(53, 122, 133, 22);
 		pnl_AgregarPersona.add(lbl_IntMusica);
 
 		JLabel lbl_IntEspectaculo = new JLabel("Interes Espectaculo");
 		lbl_IntEspectaculo.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lbl_IntEspectaculo.setBounds(52, 120, 133, 22);
+		lbl_IntEspectaculo.setBounds(53, 149, 133, 22);
 		pnl_AgregarPersona.add(lbl_IntEspectaculo);
 
 		JLabel lbl_IntCiencia = new JLabel("Interes Ciencia");
 		lbl_IntCiencia.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lbl_IntCiencia.setBounds(52, 147, 133, 22);
+		lbl_IntCiencia.setBounds(53, 176, 133, 22);
 		pnl_AgregarPersona.add(lbl_IntCiencia);
 		
 		txt_Id= new JTextField();
-		txt_Id.setBounds(184, 39, 282, 22);
+		txt_Id.setEditable(false);
+		txt_Id.setBounds(185, 40, 282, 22);
 		pnl_AgregarPersona.add(txt_Id);
 		
 		txt_Nombre = new JTextField();
-		txt_Nombre.setBounds(184, 39, 282, 22);
+		txt_Nombre.setBounds(185, 68, 282, 22);
 		pnl_AgregarPersona.add(txt_Nombre);
 		
 		cbx_Deporte = new JComboBox<Integer>();
-		cbx_Deporte.setBounds(184, 66, 282, 22);
+		cbx_Deporte.setBounds(185, 95, 282, 22);
 		pnl_AgregarPersona.add(cbx_Deporte);
 		
 		cbx_Musica = new JComboBox<Integer>();
-		cbx_Musica.setBounds(184, 93, 282, 22);
+		cbx_Musica.setBounds(185, 122, 282, 22);
 		pnl_AgregarPersona.add(cbx_Musica);
 		
 		cbx_Espectaculo = new JComboBox<Integer>();
-		cbx_Espectaculo.setBounds(184, 120, 282, 22);
+		cbx_Espectaculo.setBounds(185, 149, 282, 22);
 		pnl_AgregarPersona.add(cbx_Espectaculo);
 
 		cbx_Ciencia = new JComboBox<Integer>();
-		cbx_Ciencia.setBounds(184, 147, 282, 22);
+		cbx_Ciencia.setBounds(185, 176, 282, 22);
 		pnl_AgregarPersona.add(cbx_Ciencia);
 		
 		for (int i = rangoMinimo; i <= rangoMaximo; i++) {
@@ -146,7 +147,7 @@ public class VentanaEditarPersona extends JFrame {
 
 		JButton btn_Actualizar = new JButton("Actualizar");
 		btn_Actualizar.setFont(new Font("Tahoma", Font.BOLD, 10));
-		btn_Actualizar.setBounds(152, 179, 100, 30);
+		btn_Actualizar.setBounds(81, 218, 100, 30);
 		btn_Actualizar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -168,7 +169,7 @@ public class VentanaEditarPersona extends JFrame {
 		
 		JButton btn_Eliminar = new JButton("Eliminar");
 		btn_Eliminar.setFont(new Font("Tahoma", Font.BOLD, 10));
-		btn_Eliminar.setBounds(271, 179, 100, 30);
+		btn_Eliminar.setBounds(200, 218, 100, 30);
 		btn_Eliminar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -180,7 +181,7 @@ public class VentanaEditarPersona extends JFrame {
 		
 		JButton btn_Cancelar = new JButton("Cancelar");
 		btn_Cancelar.setFont(new Font("Tahoma", Font.BOLD, 10));
-		btn_Cancelar.setBounds(271, 179, 100, 30);
+		btn_Cancelar.setBounds(320, 218, 100, 30);
 		btn_Cancelar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
